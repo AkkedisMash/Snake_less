435643b1467e67e288b88a69722f900feb41a0e4
AkkedisMash <morok-ne@mail.ru>
2017-07-12 02:47:05 +0300

    Рисуем Символы

--- Snake_less/Snake_less/Program.cs
+++ Snake_less/Snake_less/Program.cs
@@ -10,7 +10,16 @@ namespace Snake_less
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int x1 = 3;
+            int y1 = 6;
+            char sym = '#';
+            Draw(x1, y1, sym);
+            Console.ReadKey();
+        }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
         }
     }
 }
